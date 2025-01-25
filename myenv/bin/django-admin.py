bc87302873ff94ebee5e0273784a3533a320d6ae
Althaf B Meledath <althafbmeledath@Althafs-MacBook-Air.local>
2025-01-26 01:40:59 +0530

    Initial commit

---
+++ myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/althafbmeledath/Downloads/college Projects/vehicleservicemanagement-master/myenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
